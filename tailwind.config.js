34a12626ecfb93c6df03946122865de918bc2c1c
shuvo <shuvokaron85@gmail.com>
2023-08-20 02:47:11 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors:{
+      'primary-color':'#ff5f1b',
+      'text-color': '#FFFFFF',
+      'background-footer':'black',
+      'text-footer': 'white',
+    },
+    fontFamily:{
+
+    },
+    extend: {},
+  },
+  plugins: [],
+}
